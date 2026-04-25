86a823a5deb2eb86ca20cdab1795ea9628afcb15
Ibrahim <feriziibrahim824@gmail.com>
2026-04-25 11:41:35 +0200

    upgraded formulas

--- pulling_data/pullling_data.py
+++ pulling_data/pullling_data.py
@@ -16,15 +16,15 @@ from sentinelhub import (
 
 # NOT LOGGED-IN
 
-config = SHConfig()
-config.sh_client_id = getpass.getpass("Enter your SentinelHub client id")
-config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
-config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
-config.sh_base_url = "https://sh.dataspace.copernicus.eu"
-config.save("cdse")
+# config = SHConfig()
+# config.sh_client_id = getpass.getpass("Enter your SentinelHub client id")
+# config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
+# config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+# config.sh_base_url = "https://sh.dataspace.copernicus.eu"
+# config.save("cdse")
 
 # ALREADY LOGGED-IN
-# config = SHConfig()
+config = SHConfig()
 
 SENTINEL2_CDSE = DataCollection.SENTINEL2_L2A.define_from(
     "SENTINEL2_L2A_CDSE",
