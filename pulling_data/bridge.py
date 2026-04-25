30be4e357f5c2782cb900a85aa96f7c80e1bcd80
ficoBS <filipjovanovski006@gmail.com>
2026-04-25 19:05:09 +0200

    final bridge

--- pulling_data/bridge.py
+++ pulling_data/bridge.py
@@ -20,7 +20,6 @@ crop_mapping = {
     "Spinach": [0.7, 1.00, 0.95],
     "Cucumber": [0.5, 1.00, 0.80],
     "Almond": [0.40, 0.90, 0.65**16],
-    "Egg Plant": [0.6, 1.05, 0.90],
     "Peach": [0.45, 0.90, 0.65**16]
 }
 
@@ -29,7 +28,6 @@ crop_list = list(crop_mapping.keys())
 
 @app.route('/api/crops', methods=['GET'])
 async def get_crops():
-    crop_list = list(crop_mapping.keys())
     return jsonify({
         'status': 'success',
         'crops': crop_list
